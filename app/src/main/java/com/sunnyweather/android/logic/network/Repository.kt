8a8a593445e0b69b31c484fec69b64aa17a9ca20
GuXiangLi <2645199099@qq.com>
2024-05-11 17:00:05 +0800

    实现搜索全球城市数据功能

---
+++ app/src/main/java/com/sunnyweather/android/logic/network/Repository.kt
@@ -0,0 +1,25 @@
+package com.sunnyweather.android.logic.network
+
+import androidx.lifecycle.liveData
+import com.sunnyweather.android.logic.model.Place
+import com.sunnyweather.android.logic.network.SunnyWeatherNetwork
+import kotlinx.coroutines.Dispatchers
+
+//仓库层(数据获取与缓存的中间层): 去网络层获取最新数据
+object Repository {
+    //指定函数线程参数, 在子线程中运行
+    fun searchPlaces(query: String) = liveData(Dispatchers.IO) {
+        val result = try {
+            val placeResponse = SunnyWeatherNetwork.searchPlaces(query)
+            if (placeResponse.status == "ok") {
+                val places = placeResponse.places
+                Result.success(places)
+            } else {
+                Result.failure(RuntimeException("response status is ${placeResponse.status}"))
+            }
+        } catch (e: Exception) {
+            Result.failure<List<Place>>(e)
+        }
+        emit(result)
+    }
+}
---
+++ app/src/main/java/com/sunnyweather/android/ui/place/PlaceFragment.kt
@@ -0,0 +1,74 @@
+package com.sunnyweather.android.ui.place
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.sunnyweather.android.R
+import com.sunnyweather.android.databinding.FrangmentPlaceBinding
+import com.sunnyweather.android.databinding.PlaceItemBinding
+import com.sunnyweather.android.logic.model.Place
+
+class PlaceFragment : Fragment() {
+    // This property is only valid between onCreateView and onDestroyView.
+    private val binding get() = _binding!!
+    private var _binding: FrangmentPlaceBinding? = null
+
+    private val viewModel by lazy {
+        ViewModelProviders.of(this).get(PlaceViewModel::class.java)
+    }
+
+    private lateinit var adapter: PlaceAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FrangmentPlaceBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val layoutManager = LinearLayoutManager(activity)
+        binding.recyclerView.layoutManager = layoutManager
+        adapter = PlaceAdapter(this, viewModel.placeList)
+        binding.recyclerView.adapter = adapter
+        binding.searchPlaceEdit.addTextChangedListener { editable ->
+            val content = editable.toString()
+            if (content.isNotEmpty()) {
+                viewModel.searchPlaces(content)
+            } else {
+                binding.recyclerView.visibility = View.GONE
+                binding.bgImageView.visibility = View.VISIBLE
+                viewModel.placeList.clear()
+                adapter.notifyDataSetChanged()
+            }
+        }
+        viewModel.placeLiveData.observe(viewLifecycleOwner, Observer { result ->
+            val places = result.getOrNull()
+            if (places != null) {
+                binding.recyclerView.visibility = View.VISIBLE
+                binding.bgImageView.visibility = View.GONE
+                viewModel.placeList.clear()
+                viewModel.placeList.addAll(places)
+                adapter.notifyDataSetChanged()
+            } else {
+                Toast.makeText(activity, "未能查询到任何地点", Toast.LENGTH_SHORT).show()
+                result.exceptionOrNull()?.printStackTrace()
+            }
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
